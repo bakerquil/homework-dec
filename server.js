1b08fa8cb88442df09c16b841b7c998d8d38d7c0
Baker Quillin <bakerquil@gmail.com>
2020-12-13 17:57:48 -0500

    changed to jaws db url

--- server.js
+++ server.js
@@ -1,46 +1,26 @@
-const mysql = require("mysql");
-const express = require("express");
-const app = express();
+var express = require("express");
 
-const PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
 
 app.use(express.static("public"));
+
 app.use(express.urlencoded({extended: true}));
+
 app.use(express.json());
+
 var exphbs = require("express-handlebars");
+
 app.engine("handlebars",exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
+
+
 var routes = require("./controllers/burgers_controller.js")
-app.use(routes);
 
-
-let connection;
-if (process.env.JAWSDB_URL){
-connection = mysql.createConnection(process.env.JAWSDB_URL);
-
-} else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: "",
-        database: "burgers_db"
-    });
-
-}
-
-
-connection.connect(function(err){
-    if (err) throw err;
-    console.log("connected")
-    connection.end();
-});
-app.get("/",function(req,res){
-    res.send("connected");
-})
+app.use(routes)
 
 app.listen(PORT, function(){
-    console.log("you've connected at http://localhost:" + PORT);
+    console.log("app now listening at https/localhost://" + PORT);
 });
-
-
