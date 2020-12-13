5c5e3407e9c216b59ae39d29cab15d329a4f882e
Baker Quillin <bakerquil@gmail.com>
2020-12-13 17:31:52 -0500

    changed server.js

--- server.js
+++ server.js
@@ -1,26 +1,46 @@
-var express = require("express");
+const mysql = require("mysql");
+const express = require("express");
+const app = express();
 
-var PORT = process.env.PORT || 3000;
-
-var app = express();
-
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static("public"));
-
 app.use(express.urlencoded({extended: true}));
-
 app.use(express.json());
-
 var exphbs = require("express-handlebars");
-
 app.engine("handlebars",exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
-
-
 var routes = require("./controllers/burgers_controller.js")
+app.use(routes);
 
-app.use(routes)
+
+let connection;
+if (process.env.JAWSDB_URL){
+connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "",
+        database: "animals_db"
+    });
+
+}
+
+
+connection.connect(function(err){
+    if (err) throw err;
+    console.log("connected")
+    connection.end();
+});
+app.get("/",function(req,res){
+    res.send("connected");
+})
 
 app.listen(PORT, function(){
-    console.log("app now listening at https/localhost://" + PORT);
+    console.log("you've connected at http://localhost:" + PORT);
 });
+
+
